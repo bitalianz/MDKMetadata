d4bf6edb409002cbadd7c9fef3e0a51ef1190906
Diego Caicedo <dgcaicedo@ayesa.com>
2020-03-28 17:37:09 +0000

    Initial Commit

---
+++ metadata/customsapassetmanagervanti.SAPAssetManager/Rules/vanti/SalirdePaginas.js
@@ -0,0 +1,13 @@
+export default function SalirdePaginas(context) {
+	return context.executeAction('/SAPAssetManager/Actions/Page/ClosePage.action').then(() => {
+		return context.executeAction('/SAPAssetManager/Actions/Page/ClosePage.action').then(() => {
+			return context.executeAction('/SAPAssetManager/Actions/Page/ClosePage.action').then(() => {
+				return context.executeAction('/SAPAssetManager/Actions/Page/ClosePage.action').then(() => {
+					return context.executeAction('/SAPAssetManager/Actions/Page/ClosePage.action').then(() => {
+						return context.executeAction('/SAPAssetManager/Actions/Page/ClosePage.action');
+					});
+				});
+			});
+		});
+	});
+}
